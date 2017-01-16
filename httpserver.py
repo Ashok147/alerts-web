b01737a421a5afcd505b7a0969d888903f53e8c9
Pavan Kumar <pavan.tummalapalli@gmail.com>
2017-01-16 09:25:59 +0530

    Update httpserver.py

--- httpserver.py
+++ httpserver.py
@@ -1,9 +1,13 @@
 import SimpleHTTPServer
 import SocketServer
+from SocketServer import ThreadingMixIn
 
 PORT = 8000
 
 Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+class ThreadedHTTPServer(ThreadingMixIn, SimpleHTTPServer):
+    """Handle requests in a separate thread."""
 
 httpd = SocketServer.TCPServer(("", PORT), Handler)
 
